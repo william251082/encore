4e29dca4584de3f69e9f1c9551142a07700c12e9
william <william.del.rosario@iceshop.nl>
2019-09-21 16:42:10 +0200

    Entry components -- made entry components to structure js code

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ Encore
     .setPublicPath('/build')
 
     // will create public/build/rep_log.js
-    .addEntry('rep_log', './public/assets/js/RepLogApp.js')
+    .addEntry('rep_log', './public/assets/js/rep_log.js')
 
     .enableBuildNotifications()
 ;
