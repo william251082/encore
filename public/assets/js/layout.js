9f85fb8d5521d70a65eaf9d2b5093bf5be573fb2
william <william.del.rosario@iceshop.nl>
2019-09-21 17:41:34 +0200

    Require css

--- public/assets/js/layout.js
+++ public/assets/js/layout.js
@@ -2,6 +2,9 @@
 
 const $ = require('jquery');
 require('bootstrap');
+require('bootstrap/dist/css/bootstrap.css');
+require('font-awesome/css/font-awesome.css');
+require('../css/main.css');
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
